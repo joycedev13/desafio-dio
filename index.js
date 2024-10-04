68b49d2db84aeadef29c734b51dd3834b64e9c28
Joyce <joycecarreiro13@gmail.com>
2024-10-04 20:45:38 -0300

    desafio finalizado

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-jnjnjk
+let nomeHeroi = "Joyce";
+let xpHeroi = 10001;
+let nivel;
+
+if (xpHeroi <= 1000){
+    nivel = "Ferro"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel + "." );
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel + "." );
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel + "." );
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel + "." );
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel + "." );
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel + "." );
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel + "." );
+} else {
+    nivel = "Radiante"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel + "." );
+}
